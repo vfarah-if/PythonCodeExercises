8db0e0da5e6d110b019cd0ef986ab0fc43e95a47
Vincent Farah <vincent.farah@madetech.com>
2021-01-23 10:40:05 +0000

    Added key error test

--- project_example/tests/phonebook/test_phonebook.py
+++ project_example/tests/phonebook/test_phonebook.py
@@ -5,5 +5,13 @@ class PhoneBookTest(unittest.TestCase):
     def test_lookup_by_name(self):
         phonebook = PhoneBook()
         phonebook.add("Bob", "12345")
+        
         number = phonebook.lookup("Bob")
+        
         self.assertEqual("12345", number)
+
+    def test_raises_key_error_when_name_not_found(self):
+        phonebook = PhoneBook()
+
+        with self.assertRaises(KeyError):
+            phonebook.lookup("UserThatDoesNotExist")
