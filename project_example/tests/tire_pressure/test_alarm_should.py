558a622d590c9b11da7c69cf5e79f2256aea4062
Vincent Farah <vincent.farah@madetech.com>
2021-01-25 17:16:56 +0000

    Refactored with stub using mock framework

--- project_example/tests/tire_pressure/test_alarm_should.py
+++ project_example/tests/tire_pressure/test_alarm_should.py
@@ -1,23 +1,32 @@
+from tire_pressure.sensor import Sensor
 from tire_pressure.alarm import Alarm
+from unittest.mock import Mock
 
 
 def test_alarm_is_off_by_default(alarm: Alarm):
     assert alarm.isAlarmOn == False
 
+
 class StubLowPressureSensor:
-      def samplePressure(self):
-           return 10
+    def samplePressure(self):
+        return 10
+
 
 class StubHighPressureSensor:
-      def samplePressure(self):
-           return 100
+    def samplePressure(self):
+        return 100
+
 
 class StubNormalPressureSensor:
-      def samplePressure(self):
-           return 18
+    def samplePressure(self):
+        return 18
+
 
 def test_low_pressure_activates_alarm():
-    alarm = Alarm(sensor = StubLowPressureSensor())
+    # alarm = Alarm(sensor = StubLowPressureSensor())
+    stubSensor = Mock(Sensor)
+    stubSensor.samplePressure.return_value = 10
+    alarm = Alarm(sensor=stubSensor)
 
     alarm.check()
 
@@ -25,15 +34,21 @@ def test_low_pressure_activates_alarm():
 
 
 def test_high_pressure_activates_alarm():
-    alarm = Alarm(sensor = StubHighPressureSensor())
+    # alarm = Alarm(sensor=StubHighPressureSensor())
+    stubSensor = Mock(Sensor)
+    stubSensor.samplePressure.return_value = 100
+    alarm = Alarm(sensor=stubSensor)
 
     alarm.check()
 
     assert alarm.isAlarmOn == True
 
-def test_valid_pressure_does_notactivates_alarm():
-    alarm = Alarm(sensor = StubNormalPressureSensor())
 
+def test_valid_pressure_does_notactivates_alarm():
+    # alarm = Alarm(sensor=StubNormalPressureSensor())
+    stubSensor = Mock(Sensor)
+    stubSensor.samplePressure.return_value = 18
+    alarm = Alarm(sensor=stubSensor)
     alarm.check()
 
     assert alarm.isAlarmOn == False
