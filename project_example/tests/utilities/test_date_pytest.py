50fa4d87385310f01851c98410c39130f573a7ae
Vincent Farah <vincent.farah@madetech.com>
2021-01-25 07:32:58 +0000

    Created forst simple pytest

---
+++ project_example/tests/utilities/test_date_pytest.py
@@ -0,0 +1,16 @@
+from datetime import date
+from utilities.date import daysAgo
+import pytest
+
+# @pytest.skip('actual.day failing for some odd reason')
+
+def test_date_generated_is_correct():
+    dayCount = 2
+    today = date.today()
+    expectedDay = today.day - dayCount
+
+    actual = daysAgo(days=dayCount)
+
+    assert actual.day == expectedDay
+    assert actual.month == today.month
+    assert actual.year == today.year
--- project_example/tests/utilities/test_date_unittest.py
+++ project_example/tests/utilities/test_date_unittest.py
@@ -4,10 +4,11 @@ from utilities.date import daysAgo
 
 class DaysAgoShould(TestCase):
     def test_date_generated_is_correct(self):
-        dayCount = 2
-        actual = daysAgo(dayCount)
+        dayCount = 2        
         today = date.today()
 
+        actual = daysAgo(dayCount)
+        
         self.assertEqual(actual.day, today.day - dayCount)
         self.assertEqual(actual.month, today.month)
         self.assertEqual(actual.year, today.year)
