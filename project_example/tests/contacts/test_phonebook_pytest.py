658da80bc7680ae295d6088a47e21f6c5980f7b1
Vincent Farah <vincent.farah@madetech.com>
2021-01-25 08:40:50 +0000

    Added exception testing test

--- project_example/tests/contacts/test_phonebook_pytest.py
+++ project_example/tests/contacts/test_phonebook_pytest.py
@@ -12,9 +12,24 @@ def test_add_creates_a_phone_book_entry():
 
 def test_add_number_phone_book_entry():
     phonebook = PhoneBook()
+
     phonebook.add("Bob", 1234)
 
     assert len(phonebook.phoneNumbers) == 1
     assert phonebook.phoneNumbers == {"Bob": 1234}
 
 
+def test_lookup_by_name():
+    phonebook = PhoneBook()
+    phonebook.add("Bob", 1234)
+
+    actual = phonebook.lookup("Bob")
+
+    assert actual == 1234
+
+
+def test_raises_key_error_when_name_not_found():
+    phonebook = PhoneBook()
+
+    with pytest.raises(KeyError):
+        phonebook.lookup("UserThatDoesNotExist")
