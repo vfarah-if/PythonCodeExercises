151aa351766d0eb0a060b768bb86594ec82d4259
Vincent Farah <vincent.farah@madetech.com>
2021-01-26 16:09:06 +0000

    New test for 1 - 1

--- project_example/tests/tennis/test_scoring_should_pytest.py
+++ project_example/tests/tennis/test_scoring_should_pytest.py
@@ -2,9 +2,15 @@
 def tennisScore(playerOneScore: int, playerTwoScore: int):
     if playerOneScore == 0 and playerTwoScore == 0:
         return "Love-All"
+    elif playerOneScore == 1 and playerTwoScore == 1:
+        return "Fifteen-All" 
 
 
 def test_0_0_love_all():
     actual = tennisScore(0, 0)
     assert actual == "Love-All"
 
+
+def test_1_1_fifteen_all():
+    actual = tennisScore(1, 1)
+    assert actual == "Fifteen-All"
