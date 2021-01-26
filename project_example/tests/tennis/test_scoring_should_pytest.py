d4aeafb0df36c5f8149e39c6ad0bf684e882a998
Vincent Farah <vincent.farah@madetech.com>
2021-01-26 16:29:35 +0000

    Refactored to reduce the amount of code Added param test

--- project_example/tests/tennis/test_scoring_should_pytest.py
+++ project_example/tests/tennis/test_scoring_should_pytest.py
@@ -1,16 +1,26 @@
 
+import pytest
+
+
 def tennisScore(playerOneScore: int, playerTwoScore: int):
-    if playerOneScore == 0 and playerTwoScore == 0:
-        return "Love-All"
-    elif playerOneScore == 1 and playerTwoScore == 1:
-        return "Fifteen-All" 
+    scoreNames = ["Love", "Fifteen"]
+    return scoreNames[playerOneScore]+"-All"
 
 
-def test_0_0_love_all():
-    actual = tennisScore(0, 0)
-    assert actual == "Love-All"
+@pytest.mark.parametrize("playerOneScore, playerTwoScore, expectedResult",
+                         [(0, 0, "Love-All"),
+                          (1, 1, "Fifteen-All"),
+                          ])
+def test_scoring(playerOneScore: int, playerTwoScore: int, expectedResult: str):
+    actualScore = tennisScore(playerOneScore, playerTwoScore)
+    assert actualScore == expectedResult
 
 
-def test_1_1_fifteen_all():
-    actual = tennisScore(1, 1)
-    assert actual == "Fifteen-All"
+# def test_0_0_love_all():
+#     actual = tennisScore(0, 0)
+#     assert actual == "Love-All"
+
+
+# def test_1_1_fifteen_all():
+#     actual = tennisScore(1, 1)
+#     assert actual == "Fifteen-All"
