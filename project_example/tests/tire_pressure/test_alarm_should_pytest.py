9ad4df5f4de0495601a77052219825408c58f152
Vincent Farah <vincent.farah@madetech.com>
2021-01-28 07:52:26 +0000

    Rename variables the way it should be

--- project_example/tests/tire_pressure/test_alarm_should_pytest.py
+++ project_example/tests/tire_pressure/test_alarm_should_pytest.py
@@ -4,32 +4,33 @@ from unittest.mock import Mock, patch
 
 
 def test_alarm_is_off_by_default(alarm: Alarm):
-    assert alarm.is_alarm_on == False
+    assert alarm.is_alarm_on is False
 
 
 def test_low_pressure_activates_alarm():
-    alarm = Alarm(sensor=mockSensor(10))
+    alarm = Alarm(sensor=mock_sensor(10))
 
     alarm.check()
 
-    assert alarm.is_alarm_on == True
+    assert alarm.is_alarm_on is True
 
 
 def test_high_pressure_activates_alarm():
-    alarm = Alarm(sensor=mockSensor(100))
+    alarm = Alarm(sensor=mock_sensor(100))
 
     alarm.check()
 
-    assert alarm.is_alarm_on == True
+    assert alarm.is_alarm_on is True
 
 
 def test_call_sensor_sample_pressure():
-    sensorSpy = mockSensor(100)
-    alarm = Alarm(sensor=sensorSpy)
+    sensor_spy = mock_sensor(100)
+    alarm = Alarm(sensor=sensor_spy)
 
     alarm.check()
 
-    sensorSpy.sample_pressure.assert_called_once()
+    sensor_spy.sample_pressure.assert_called_once()
+
 
 # REMARKS: Patch makes more sense if the constructor does not allow for sensor to be assigned
 
@@ -37,37 +38,37 @@ def test_call_sensor_sample_pressure():
 def test_high_pressure_activates_alarm_using_monkeypatch():
     # REMARKS refers to the modules in the patch param
     with patch("tire_pressure.alarm.Sensor") as sensorType:
-        sensorInstance = Mock()
-        sensorInstance.sample_pressure.return_value = 22
-        sensorType.return_value = sensorInstance
+        mock_sensor = Mock(Sensor)
+        mock_sensor.sample_pressure.return_value = 22
+        sensorType.return_value = mock_sensor
 
         alarm = Alarm()
         alarm.check()
 
-        assert alarm.is_alarm_on == True
+        assert alarm.is_alarm_on is True
 
 
 @patch("tire_pressure.alarm.Sensor")
-def test_low_pressure_activates_alarm_using_monkeypatch_decorator(sensorType):
-    print(sensorType)
-    sensorInstance = Mock()
-    sensorInstance.sample_pressure.return_value = 14
-    sensorType.return_value = sensorInstance
+def test_low_pressure_activates_alarm_using_monkeypatch_decorator(sensor_type):
+    print(sensor_type)
+    mock_instance = Mock(Sensor)
+    mock_instance.sample_pressure.return_value = 14
+    sensor_type.return_value = mock_instance
 
     alarm = Alarm()
     alarm.check()
 
-    assert alarm.is_alarm_on == True
+    assert alarm.is_alarm_on is True
 
 
-def test_valid_pressure_does_notactivates_alarm():
-    alarm = Alarm(sensor=mockSensor(18))
+def test_valid_pressure_does_not_activate_alarm():
+    alarm = Alarm(sensor=mock_sensor(18))
     alarm.check()
 
-    assert alarm.is_alarm_on == False
+    assert alarm.is_alarm_on is False
 
 
-def mockSensor(pressure: int):
+def mock_sensor(pressure: int):
     result = Mock(Sensor)
     result.sample_pressure.return_value = pressure
     return result
