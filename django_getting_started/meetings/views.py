a1e6b0672091f6d6e7ade6e63d0f6ac49bf90b56
Vincent Farah <vincent.farah@madetech.com>
2021-01-29 15:39:05 +0000

    Refactored to return a 404 page

--- django_getting_started/meetings/views.py
+++ django_getting_started/meetings/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from meetings.models import Meeting
 
 
 def detail(request, meeting_id):
-    meeting = Meeting.objects.get(pk=meeting_id)
+    # meeting = Meeting.objects.get(pk=meeting_id)
+    meeting = get_object_or_404(Meeting, pk=meeting_id)
     data = dict(meeting=meeting)
     return render(request, "meetings/detail.html", data)
