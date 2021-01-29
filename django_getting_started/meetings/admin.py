de58c3b2fb227aa9edd2a3b02be1d0b17457064b
Vincent Farah <vincent.farah@madetech.com>
2021-01-29 11:25:42 +0000

    Created foreign key on room

--- django_getting_started/meetings/admin.py
+++ django_getting_started/meetings/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Meeting
+from .models import Meeting, Room
 
 admin.site.register(Meeting)
+admin.site.register(Room)
--- django_getting_started/meetings/models.py
+++ django_getting_started/meetings/models.py
@@ -2,11 +2,25 @@ from datetime import time
 from django.db import models
 
 
+class Room(models.Model):
+    name = models.TextField(max_length=50)
+    floor = models.IntegerField(default=1)
+    room = models.TextField(max_length=50)
+
+    def __str__(self):
+        return f'{self.name} on floor {str(self.floor)} room {str(self.room)}'
+
+
 class Meeting(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField()
     start_time = models.TimeField(default=time(9))
     duration = models.IntegerField(default=15)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
-        return f'{self.title} @ {self.start_time.strftime("%I:%M:%S %p")} for {self.duration} minute(s)'
+        return f'{self.title} @ {self.start_time.strftime("%I:%M:%S %p")} for {self.duration} minutes {self.room_info}'
+
+    @property
+    def room_info(self):
+        return f'in room {self.room.name}' if self.room is not None else '(online)'
