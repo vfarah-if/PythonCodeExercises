62288195f6e5dfd9af81617f262a7b6fce044cf4
Vincent Farah <vincent.farah@madetech.com>
2021-01-29 09:02:50 +0000

    Updated meeting mdoel to extend understanding how to migrated

--- django_getting_started/meetings/models.py
+++ django_getting_started/meetings/models.py
@@ -1,9 +1,12 @@
+from datetime import time
 from django.db import models
 
 
 class Meeting(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField()
+    start_time = models.TimeField(default=time(9))
+    duration = models.IntegerField(default=15)
 
     def __str__(self):
-        return f'({self.id}) {self.title}'
+        return f'{self.title} @ {self.start_time.strftime("%I:%M:%S %p")} for {self.duration} minute(s)'
