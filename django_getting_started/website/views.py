bc136ea4b5985dd0a1e89fa0f20647323b424ca4
Vincent Farah <vincent.farah@madetech.com>
2021-01-30 08:00:14 +0000

    Experimented with filter and ordering

--- django_getting_started/website/views.py
+++ django_getting_started/website/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from datetime import datetime
 
 from meetings.models import Meeting, Room
 
@@ -8,7 +9,8 @@ def welcome(request):
     data = dict(
         message="Test to see the rendered using the template structure",
         meetings_count=Meeting.objects.count(),
-        meetings=Meeting.objects.all(),
+        meetings=Meeting.objects.filter(date__gte=datetime.now()).order_by('-date'),
+        archived_meetings=Meeting.objects.filter(date__lt=datetime.now()).order_by('-date'),
         rooms_count=Room.objects.count()
     )
     return render(request, 'website/welcome.html', data)
