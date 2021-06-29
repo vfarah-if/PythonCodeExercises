016bdb3df85f38fb70d5d3964f91d9674488ba0f
Vincent Farah <vincent.farah@madetech.com>
2021-06-29 21:38:02 +0100

    Minor refactor

--- project_example/game_of_life/generator.py
+++ project_example/game_of_life/generator.py
@@ -2,16 +2,23 @@ from game_of_life.cell import Cell
 
 
 class Generator:
+    """Game of life generator"""
     def __init__(self, size: int):
         self.size = size
         self.board = list()
-        for y in range(size):
+        self.initialise_board()
+
+    def initialise_board(self):
+        for y in range(self.size):
             col = list()
-            for x in range(size):
+            for x in range(self.size):
                 col.append(Cell())
             self.board.append(col)
 
     def __str__(self):
+        return self.picture_board()
+
+    def picture_board(self):
         result = ' | '
         for y in range(self.size):
             if y != 0:
