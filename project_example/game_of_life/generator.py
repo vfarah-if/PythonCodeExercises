d6e714f4f84c4d4d0043f1625a66fc894df9a76c
Vincent Farah <vincent.farah@madetech.com>
2021-06-29 22:26:15 +0100

    Generate seeded data using visualization

--- project_example/game_of_life/generator.py
+++ project_example/game_of_life/generator.py
@@ -3,17 +3,31 @@ from game_of_life.cell import Cell
 
 class Generator:
     """Game of life generator"""
-    def __init__(self, size: int):
+    def __init__(self, size: int, seed_data: list):
+        """
+        Constructor
+        @param size: Board Size or matrix setting for x and y
+        @param seed_data: List of tuples and teh tuple denoting x and y
+        """
         self.size = size
         self.board = list()
-        self.initialise_board()
+        self._initialise_board()
+        self._seed(seed_data)
 
-    def initialise_board(self):
+    def _initialise_board(self):
         for y in range(self.size):
             col = list()
             for x in range(self.size):
                 col.append(Cell())
             self.board.append(col)
+
+    def _seed(self, positions: list):
+        for item in positions:
+            x = item[0]
+            y = item[1]
+            cell = self.board[x][y]
+            if cell is not None:
+                cell.is_alive = True
 
     def __str__(self):
         return self.picture_board()
--- project_example/tests/game_of_life/test_generator_should.py
+++ project_example/tests/game_of_life/test_generator_should.py
@@ -3,12 +3,25 @@ from os import linesep
 
 
 class TestGeneratorShould:
-    def test_should_generate_a_five_by_five_empty_board(self):
+    def test_generate_a_five_by_five_empty_board(self):
         generator = Generator(5)
 
         assert str(generator) == (
-            f" |   |   |   |   |   | {linesep}"            
             f" |   |   |   |   |   | {linesep}"
+            f" |   |   |   |   |   | {linesep}"
+            f" |   |   |   |   |   | {linesep}"
+            f" |   |   |   |   |   | {linesep}"
+            f" |   |   |   |   |   | "
+        )
+
+    def test_generate_a_five_by_five_with_seeded_data(self):
+        generator = Generator(5, [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0),
+                                  (0, 1)
+                                  ])
+
+        assert str(generator) == (
+            f" | X | X | X | X | X | {linesep}"
+            f" | X |   |   |   |   | {linesep}"
             f" |   |   |   |   |   | {linesep}"
             f" |   |   |   |   |   | {linesep}"
             f" |   |   |   |   |   | "
