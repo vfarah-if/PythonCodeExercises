5adaf09fb3e225bd8c9938c15358b231406cd77e
Vincent Farah <vincent.farah@madetech.com>
2021-06-29 17:35:44 +0100

    First reproduction test

--- project_example/game_of_life/cell.py
+++ project_example/game_of_life/cell.py
@@ -3,7 +3,14 @@ class Cell:
     Represent single unit of life
     """
 
+    neighbours = list()
+
     def __init__(self, x: int, y: int, is_alive=False):
         self.x = x
         self.y = y
         self.is_alive = is_alive
+
+    def re_generate(self):
+        alive_neighbours = [neighbour for neighbour in self.neighbours if neighbour.is_alive]
+        if len(alive_neighbours) == 3:
+            self.is_alive = True
--- project_example/tests/game_of_life/test_cell_should.py
+++ project_example/tests/game_of_life/test_cell_should.py
@@ -3,10 +3,20 @@ from game_of_life.cell import Cell
 
 class TestCellShould:
 
-    def test_generate_by_default_as_a_dead_cell(self):
-        cell = Cell(0, 1, False)
+    def test_initialise_by_default_as_a_dead_cell_storing_the_position(self):
+        cell = Cell(0, 1)
 
         assert cell is not None
         assert not cell.is_alive
         assert cell.x == 0
         assert cell.y == 1
+
+    def test_come_to_life_when_three_live_neighbours_cause_reproduction(self):
+        cell = Cell(0, 1)
+        cell.neighbours.append(Cell(0, 0, True))
+        cell.neighbours.append(Cell(0, 2, True))
+        cell.neighbours.append(Cell(1, 1, True))
+
+        cell.re_generate()
+
+        cell.is_alive is True
