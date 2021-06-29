14445efa418dc22a96b6656100ae2da9fa128c1f
Vincent Farah <vincent.farah@madetech.com>
2021-06-29 18:30:49 +0100

    Refactored to make cell more readable

--- project_example/game_of_life/cell.py
+++ project_example/game_of_life/cell.py
@@ -10,8 +10,20 @@ class Cell:
         self.neighbours = list()
 
     def re_generate(self):
+        def is_overpopulated():
+            return len(alive_neighbours) > 3
+
+        def is_underpopulated():
+            return len(alive_neighbours) < 2
+
+        def is_thriving():
+            return self.is_alive and len(alive_neighbours) >= 2
+
+        def is_fertile():
+            return not self.is_alive and len(alive_neighbours) == 3
+
         alive_neighbours = [neighbour for neighbour in self.neighbours if neighbour.is_alive]
-        if (self.is_alive and len(alive_neighbours) >= 2) or (len(alive_neighbours) == 3):
+        if is_thriving() or is_fertile():
             self.is_alive = True
-        if (len(alive_neighbours) < 2) or (len(alive_neighbours) > 3):
+        if is_underpopulated() or is_overpopulated():
             self.is_alive = False
