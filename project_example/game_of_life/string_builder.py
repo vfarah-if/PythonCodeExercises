26481036f6bc2cbbd50f9c8b991bffbe5286c473
Vincent Farah <vincent.farah@madetech.com>
2021-07-20 08:33:41 +0100

    Added missing comments

--- project_example/game_of_life/string_builder.py
+++ project_example/game_of_life/string_builder.py
@@ -13,14 +13,17 @@ class StringBuilder:
             self.add(value)
 
     def add(self, value):
+        """Add a value to the output"""
         self._logger.write(value)
         return self
 
     def newline(self):
+        """Add a newline char to the output"""
         self.add(eol)
         return self
 
     def to_string(self):
+        """Output the entire output as a string"""
         return str(self)
 
     def __str__(self):
