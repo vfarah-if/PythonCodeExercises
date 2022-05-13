da59d813f60e4598df9c62330d91f8b5793c906a
Vincent Farah <vincent.farah@madetech.com>
2022-05-13 15:56:05 +0100

    Create a POC for the shared tasks decorator

--- project_example/decorators/printer.py
+++ project_example/decorators/printer.py
@@ -14,3 +14,23 @@ def my_decorator(arg):
 def printer(a, b):
     print('in function')
     return a + b
+
+
+def shared_task(**options):
+    def create_shared_task(bind=False, priority=1, queue='long-running'):
+        def _expose_caller(func):
+            def _expose_caller_args(*args, **kwargs):
+                print('Before running task', args, kwargs, bind, priority, queue)
+                result = func(*args, **kwargs)
+                print('After running task', result)
+                return result
+            return _expose_caller_args
+        return _expose_caller
+
+    return create_shared_task(**options)
+
+
+@shared_task(bind=True, priority=9, queue='short-running')
+def handler(a, b):
+    print('Running task', a, b)
+    return a + b
--- project_example/tests/decorators/test_printers_should.py
+++ project_example/tests/decorators/test_printers_should.py
@@ -1,7 +1,7 @@
 import io
 import sys
 
-from decorators.printer import printer
+from decorators.printer import printer, handler
 
 
 def test_passing_arguments_to_decorators():
@@ -11,4 +11,14 @@ def test_passing_arguments_to_decorators():
     actual = printer(1, 2)
 
     assert actualOutput.getvalue() == 'before function\nin function\nafter function\n'
+    assert actual == 3
+
+
+def test_task_execution_of_handler():
+    actualOutput = io.StringIO()          # Create StringIO object
+    sys.stdout = actualOutput
+
+    actual = handler(1, 2)
+
+    assert actualOutput.getvalue() == "Before running task (1, 2) {} {'bind': True, 'priority': 9, 'queue': 'short-running'}\nRunning task 1 2\nAfter running task 3\n"
     assert actual == 3
